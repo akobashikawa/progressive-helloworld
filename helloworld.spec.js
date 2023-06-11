31f224508d6df0df0f457f9f5a3176e4f4e8b69b
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-06-11 11:27:39 -0500

    Agrega describe

--- helloworld.spec.js
+++ helloworld.spec.js
@@ -1,6 +1,10 @@
 const { helloworld } = require ('./helloworld');
 
-test('El saludo es mostrado', () => {
-    const result = helloworld();
-    expect(result).toBe('Hello World');
-})
+describe('El saludo es mostrado', () => {
+
+    test('El saludo es mostrado', () => {
+        const result = helloworld();
+        expect(result).toBe('Hello World');
+    });
+
+});
