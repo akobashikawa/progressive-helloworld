0d6d29a51b8349ae6780c775805b2a437c30eb2a
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-06-14 22:25:21 -0500

    Add dotenv support and use dynamic port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -5,7 +6,7 @@ const apiRouter = require('./api/apiRouter');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || '3000';
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
